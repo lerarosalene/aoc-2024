2e8545d3596a4a5d6dca9a1a34d71bdfdbcde77a
Larissa Rosalene <lerarosalene@outlook.com>
2024-12-24 15:38:51 +0300

    day 24: rework circuit compiler, minor optimizations

--- src/days/day-24/compile.ts
+++ src/days/day-24/compile.ts
@@ -1,113 +1,58 @@
-import assert from "node:assert";
-import { Formula, Operator, Wire } from "./common";
+import { Formula, Operator } from "./common";
 
-interface WireNode {
-  type: "wire";
-  name: string;
-}
-
-interface OperatorNode {
-  type: "operator";
-  left: FormulaNode;
-  right: FormulaNode;
-  operator: Operator;
-}
-
-type FormulaNode = WireNode | OperatorNode;
-
-export class Resolver {
-  constructor(private _map: Map<string, Formula>) {}
-  private _cache = new Map<string, FormulaNode>();
-
-  public resolve(wire: string, visited = new Set<Formula>()): FormulaNode {
-    const cached = this._cache.get(wire);
-    if (cached !== undefined) {
-      return cached;
-    }
-    if (/^(?:x|y)\d\d$/.test(wire)) {
-      return { type: "wire", name: wire };
-    }
-
-    const formula = this._map.get(wire);
-    assert(formula);
-    if (visited.has(formula)) {
-      throw new Error("Cycle detected");
-    }
-    const result: FormulaNode = {
-      type: "operator",
-      operator: formula.operator,
-      left: this.resolve(formula.left, new Set([...visited, formula])),
-      right: this.resolve(formula.right, new Set([...visited, formula])),
-    };
-    this._cache.set(wire, result);
-    return result;
+function compileOperator(operator: Operator) {
+  switch (operator) {
+    case "AND":
+      return "&&";
+    case "OR":
+      return "||";
+    case "XOR":
+      return "!==";
+    default:
+      let _: never = operator;
+      throw new Error("Unknown operator " + operator);
   }
 }
 
-class Evaluator {
-  constructor(inputs: Wire[]) {
-    for (const wire of inputs) {
-      this._inputs.set(wire.name, wire.value);
+type Func = (
+  inputs: Partial<Record<string, boolean>>,
+) => Partial<Record<string, boolean>>;
+
+export function compile(formulas: Formula[]): Func {
+  let result: string[] = [];
+
+  result.push(`let nodes = {...inputs};`);
+
+  let remaining: Formula[] = formulas;
+  let computed = new Set<string>();
+
+  let loopCanary = false;
+  while (remaining.length > 0) {
+    if (loopCanary) {
+      throw new Error("Circuit can not be compiled. Check for loops");
     }
+    loopCanary = true;
+    let next: Formula[] = [];
+    for (const formula of remaining) {
+      const isLeftComputed =
+        computed.has(formula.left) || /^(?:x|y)\d\d$/.test(formula.left);
+      const isRightComputed =
+        computed.has(formula.right) || /^(?:x|y)\d\d$/.test(formula.right);
+      const canBeComputed = isLeftComputed && isRightComputed;
+      if (canBeComputed) {
+        result.push(
+          `nodes["${formula.result}"] = nodes["${formula.left}"] ${compileOperator(formula.operator)} nodes["${formula.right}"];`,
+        );
+        computed.add(formula.result);
+        loopCanary = false;
+      } else {
+        next.push(formula);
+      }
+    }
+    remaining = next;
   }
 
-  private _inputs = new Map<string, boolean>();
-  private _cache = new Map<FormulaNode, boolean>();
+  result.push(`return nodes;`);
 
-  public evaluate(node: FormulaNode): boolean {
-    const cached = this._cache.get(node);
-    if (cached !== undefined) {
-      return cached;
-    }
-
-    if (node.type === "wire") {
-      const result = this._inputs.get(node.name);
-      assert(result !== undefined);
-      this._cache.set(node, result);
-      return result;
-    }
-
-    const lhs = this.evaluate(node.left);
-    const rhs = this.evaluate(node.right);
-    let result: boolean | null = null;
-    switch (node.operator) {
-      case "AND":
-        result = lhs && rhs;
-        break;
-      case "OR":
-        result = lhs || rhs;
-        break;
-      case "XOR":
-        result = lhs !== rhs;
-        break;
-      default:
-        let _: never = node.operator;
-        throw new Error("Unreachable");
-    }
-
-    assert(result !== undefined);
-    this._cache.set(node, result);
-    return result;
-  }
-}
-
-export function compile(formulas: Formula[]) {
-  const formulaMap = new Map<string, Formula>();
-  for (const formula of formulas) {
-    formulaMap.set(formula.result, formula);
-  }
-
-  return new Resolver(formulaMap);
-}
-
-export function evaluate(resolver: Resolver, input: Wire[]) {
-  const evaluator = new Evaluator(input);
-  let result = 0;
-
-  for (let i = 45; i >= 0; --i) {
-    const wire = `z${i.toString().padStart(2, "0")}`;
-    result = result * 2 + (evaluator.evaluate(resolver.resolve(wire)) ? 1 : 0);
-  }
-
-  return result;
+  return new Function("inputs", result.join("\n")) as Func;
 }
--- src/days/day-24/index.ts
+++ src/days/day-24/index.ts
@@ -1,9 +1,9 @@
 import assert from "node:assert";
 import { lines } from "../../common/utils";
 import { Formula, isOperator, Wire } from "./common";
-import { compile, evaluate } from "./compile";
 import { FormulaLookup } from "./lookup";
 import { validateAdders } from "./validate";
+import { compile } from "./compile";
 
 function parseWires(wires: string) {
   return lines(wires).map((line): Wire => {
@@ -39,9 +39,23 @@ export function parse(input: string) {
 
 export function partOne(input: string) {
   const { wires, formulas } = parse(input);
-  const resolver = compile(formulas);
+  const f = compile(formulas);
+  const inputs = wires.reduce(
+    (acc, wire) => {
+      acc[wire.name] = wire.value;
+      return acc;
+    },
+    {} as Partial<Record<string, boolean>>,
+  );
 
-  return evaluate(resolver, wires);
+  const outputs = f(inputs);
+  let result = 0;
+  for (let i = 45; i >= 0; --i) {
+    result =
+      result * 2 + (outputs[`z${i.toString().padStart(2, "0")}`] ? 1 : 0);
+  }
+
+  return result;
 }
 
 export function partTwo(input: string) {
@@ -53,14 +67,20 @@ export function partTwo(input: string) {
     .filter((wire) => !correct.has(wire));
 
   let fixes = new Map<number, [string, string]>();
+  let tried = new Set<string>();
   for (let i = 0; i < swapCandidates.length; ++i) {
     for (let j = i + 1; j < swapCandidates.length; ++j) {
+      if (tried.has(swapCandidates[i]) || tried.has(swapCandidates[j])) {
+        continue;
+      }
       lookup.setSwaps([[swapCandidates[i], swapCandidates[j]]]);
       const { invalid: newInvalid } = validateAdders(lookup, 46, false);
       const newInvalidSet = new Set(newInvalid);
       for (const invalid of invalidAdders) {
         if (!newInvalidSet.has(invalid)) {
           fixes.set(invalid, [swapCandidates[i], swapCandidates[j]]);
+          tried.add(swapCandidates[i]);
+          tried.add(swapCandidates[j]);
         }
       }
     }
