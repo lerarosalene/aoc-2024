29018e129166fd449652e23f4c747c8543af1465
Larissa Rosalene <lerarosalene@outlook.com>
2024-12-24 12:31:13 +0300

    day 24: optimization: greatly reduce search space (~50k pairs -> 400 pairs)

--- src/days/day-24/index.ts
+++ src/days/day-24/index.ts
@@ -3,6 +3,7 @@ import { lines } from "../../common/utils";
 import { Formula, isOperator, Wire } from "./common";
 import { compile, evaluate } from "./compile";
 import { FormulaLookup } from "./lookup";
+import { validateAdders } from "./validate";
 
 function parseWires(wires: string) {
   return lines(wires).map((line): Wire => {
@@ -43,83 +44,19 @@ export function partOne(input: string) {
   return evaluate(resolver, wires);
 }
 
-function validateChainAdder(formulas: FormulaLookup, adder: number) {
-  const index = adder.toString().padStart(2, "0");
-  const x = `x${index}`;
-  const y = `y${index}`;
-  const z = `z${index}`;
-  const m = formulas.findRhs(x, y, "XOR");
-  const n = formulas.findRhs(x, y, "AND");
-  if (!m || !n) {
-    return false;
-  }
-  const prevCarry = formulas.findOperand(m, "XOR", z);
-  if (!prevCarry) {
-    return false;
-  }
-  const r = formulas.findRhs(prevCarry, m, "AND");
-  if (!r) {
-    return false;
-  }
-  const carryOut = formulas.findRhs(r, n, "OR");
-  if (!carryOut) {
-    return false;
-  }
-  return true;
-}
-
-function validateLastAdder(formulas: FormulaLookup, adder: number) {
-  const index = adder.toString().padStart(2, "0");
-  const z = `z${index}`;
-  const [carryInLeft, carryInRight] = formulas.findOperands("OR", z) ?? [];
-  if (!carryInLeft || !carryInRight) {
-    return false;
-  }
-  return true;
-}
-
-function validateFirstAdder(formulas: FormulaLookup, adder: number) {
-  const index = adder.toString().padStart(2, "0");
-  const x = `x${index}`;
-  const y = `y${index}`;
-  const i1 = formulas.findRhs(x, y, "XOR");
-  const cOut = formulas.findRhs(x, y, "AND");
-  if (i1 !== `z${index}` || !cOut) {
-    return false;
-  }
-  return true;
-}
-
-function validateAdder(formulas: FormulaLookup, adder: number, total: number) {
-  return adder === 0
-    ? validateFirstAdder(formulas, adder)
-    : adder === total - 1
-      ? validateLastAdder(formulas, adder)
-      : validateChainAdder(formulas, adder);
-}
-
-function getInvalidAdders(formulas: FormulaLookup, total: number) {
-  let result: number[] = [];
-  for (let i = 0; i < total; ++i) {
-    if (validateAdder(formulas, i, total)) {
-      continue;
-    }
-    result.push(i);
-  }
-  return result;
-}
-
 export function partTwo(input: string) {
   const { formulas } = parse(input);
   const lookup = new FormulaLookup(formulas);
-  const invalidAdders = getInvalidAdders(lookup, 46);
-  const swapCandidates = formulas.map((f) => f.result);
+  const { invalid: invalidAdders, correct } = validateAdders(lookup, 46, true);
+  const swapCandidates = formulas
+    .map((f) => f.result)
+    .filter((wire) => !correct.has(wire));
 
   let fixes = new Map<number, [string, string]>();
   for (let i = 0; i < swapCandidates.length; ++i) {
     for (let j = i + 1; j < swapCandidates.length; ++j) {
       lookup.setSwaps([[swapCandidates[i], swapCandidates[j]]]);
-      const newInvalid = getInvalidAdders(lookup, 46);
+      const { invalid: newInvalid } = validateAdders(lookup, 46, false);
       const newInvalidSet = new Set(newInvalid);
       for (const invalid of invalidAdders) {
         if (!newInvalidSet.has(invalid)) {
---
+++ src/days/day-24/validate.ts
@@ -0,0 +1,110 @@
+import { FormulaLookup } from "./lookup";
+
+function validateChainAdder(formulas: FormulaLookup, adder: number) {
+  const index = adder.toString().padStart(2, "0");
+  const x = `x${index}`;
+  const y = `y${index}`;
+  const z = `z${index}`;
+  const m = formulas.findRhs(x, y, "XOR");
+  const n = formulas.findRhs(x, y, "AND");
+  if (!m || !n) {
+    return null;
+  }
+  const prevCarry = formulas.findOperand(m, "XOR", z);
+  if (!prevCarry) {
+    return null;
+  }
+  const r = formulas.findRhs(prevCarry, m, "AND");
+  if (!r) {
+    return null;
+  }
+  const carryOut = formulas.findRhs(r, n, "OR");
+  if (!carryOut) {
+    return null;
+  }
+  return [z, m, n, r, carryOut];
+}
+
+function validateLastAdder(formulas: FormulaLookup, adder: number) {
+  const index = adder.toString().padStart(2, "0");
+  const z = `z${index}`;
+  const [carryInLeft, carryInRight] = formulas.findOperands("OR", z) ?? [];
+  if (!carryInLeft || !carryInRight) {
+    return null;
+  }
+  return [carryInLeft, carryInRight, z];
+}
+
+function validateFirstAdder(formulas: FormulaLookup, adder: number) {
+  const index = adder.toString().padStart(2, "0");
+  const x = `x${index}`;
+  const y = `y${index}`;
+  const i1 = formulas.findRhs(x, y, "XOR");
+  const cOut = formulas.findRhs(x, y, "AND");
+  if (i1 !== `z${index}` || !cOut) {
+    return null;
+  }
+  return [i1, cOut];
+}
+
+function validateAdder(formulas: FormulaLookup, adder: number, total: number) {
+  return adder === 0
+    ? validateFirstAdder(formulas, adder)
+    : adder === total - 1
+      ? validateLastAdder(formulas, adder)
+      : validateChainAdder(formulas, adder);
+}
+
+function getInvalidAdders(formulas: FormulaLookup, total: number) {
+  let result: number[] = [];
+  for (let i = 0; i < total; ++i) {
+    if (validateAdder(formulas, i, total)) {
+      continue;
+    }
+    result.push(i);
+  }
+  return result;
+}
+
+interface ValidationResultCollect {
+  invalid: Set<number>;
+  correct: Set<string>;
+}
+
+interface ValidationResultValidOnly {
+  invalid: Set<number>;
+}
+
+export function validateAdders(
+  formulas: FormulaLookup,
+  total: number,
+  collectFixedBits: true,
+): ValidationResultCollect;
+
+export function validateAdders(
+  formulas: FormulaLookup,
+  total: number,
+  collectFixedBits: false,
+): ValidationResultValidOnly;
+
+export function validateAdders(
+  formulas: FormulaLookup,
+  total: number,
+  collectFixedBits: boolean,
+) {
+  let result = new Set<number>();
+  let correct = new Set<string>();
+  for (let i = 0; i < total; ++i) {
+    let valids: string[] | null = null;
+    if ((valids = validateAdder(formulas, i, total))) {
+      if (collectFixedBits) {
+        for (const valid of valids) {
+          correct.add(valid);
+        }
+      }
+      continue;
+    }
+    result.add(i);
+  }
+  return collectFixedBits ? { invalid: result, correct } : { invalid: result };
+}
